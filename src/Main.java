f406f1273dba34d9bcab8ddf126938d71dc0fdb1
BEATRIZ OLIVEIRA ABREU DO AMARAL <beatriz.oaa@puccampinas.edu.br>
2019-08-22 11:13:12 -0300

    Lab01

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,40 @@ public class Main {
 		Scanner sc1 = new Scanner(System.in); 
 		Carro ca = new Carro();
 		Condutor co = new Condutor();
+		char op;
 		
-		System.out.println("Nome condutor: ");
-		co.setNome(sc1.nextLine());
-		System.out.println("Idade condutor: ");
-		co.setIdade(sc1.nextInt());
-		System.out.println("Naturalidade condutor: ");
-		sc1.nextLine();
-		co.setNaturalidade(sc1.nextLine());
-		
-		
-		System.out.println("Cor carroceria: ");
-		ca.setCor_carroceria(sc1.nextLine());
-		System.out.println("Qtde rodas: ");
-		ca.setQtd_rodas(sc1.nextInt());
-		System.out.println("Qtde passageiros: ");
-		ca.setQtd_passageiros(sc1.nextInt());
-		System.out.println("Num portas: ");
-		ca.setNum_portas(sc1.nextInt());
-		System.out.println("Ano fabricacao: ");
-		ca.setAno_fabricacao(sc1.nextInt());
-		System.out.println("Nome modelo: ");
-		sc1.nextLine();
-		ca.setNome_modelo(sc1.nextLine());
-		System.out.println("Nome fabricante: ");
-		ca.setNome_fabricante(sc1.nextLine());
-		
+		do {
+			System.out.println("Nome condutor: ");
+			co.setNome(sc1.nextLine());
+			System.out.println("Idade condutor: ");
+			co.setIdade(sc1.nextInt());
+			System.out.println("Naturalidade condutor: ");
+			sc1.nextLine();
+			co.setNaturalidade(sc1.nextLine());
+			
+			
+			System.out.println("Cor carroceria: ");
+			ca.setCor_carroceria(sc1.nextLine());
+			System.out.println("Qtde rodas: ");
+			ca.setQtd_rodas(sc1.nextInt());
+			System.out.println("Qtde passageiros: ");
+			ca.setQtd_passageiros(sc1.nextInt());
+			System.out.println("Num portas: ");
+			ca.setNum_portas(sc1.nextInt());
+			System.out.println("Ano fabricacao: ");
+			ca.setAno_fabricacao(sc1.nextInt());
+			System.out.println("Nome modelo: ");
+			sc1.nextLine();
+			ca.setNome_modelo(sc1.nextLine());
+			System.out.println("Nome fabricante: ");
+			ca.setNome_fabricante(sc1.nextLine());
+			
+			System.out.println("Cadastrar novamente ?");
+			op = sc1.nextLine().charAt(0);
+		} while(op == 's' || op == 'S');
 		
 		System.out.println("O condutor " + co.getNome() + " de idade " + co.getIdade()
 		+ " e naturalidade " + co.getNaturalidade() + " possui o carro de");
 		ca.informacoes();
-		
 	}
 }
